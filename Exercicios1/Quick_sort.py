78741f16c4c1fcc8f5f95bffc3cf30fcd4857265
julioCesarMorais78 <juliocesarmorais78@gmail.com>
2025-09-26 12:55:50 -0300

    id001 - alterando programa quick sort com o cursor

--- Exercicios1/Quick_sort.py
+++ Exercicios1/Quick_sort.py
@@ -1,9 +1,15 @@
-lista_valores = [5, 3, 8, 6, 2, 9, 1, 7, 4]
+# programa demonstrador do Quick sort
 
-pivot = lista_valores[4]  # Escolhendo o primeiro elemento como pivô
+def quick_sort(lista):  # define a função quick_sort que recebe uma lista
+    if len(lista) <= 1:  # verifica se a lista tem 1 ou nenhum elemento (caso base)
+        return lista  # retorna a lista se ela já está ordenada (ou vazia)
+    else:  # caso a lista tenha mais de um elemento
+        pivot = lista[len(lista) // 2]  # escolhe o elemento do meio como pivô
+        menores = [x for x in lista if x < pivot]  # cria lista com elementos menores que o pivô
+        iguais = [x for x in lista if x == pivot]  # cria lista com elementos iguais ao pivô
+        maiores = [x for x in lista if x > pivot]  # cria lista com elementos maiores que o pivô
+        return quick_sort(menores) + iguais + quick_sort(maiores)  # ordena recursivamente e concatena os resultados
 
-lista_inicial[]
+lista_valores = [5, 3, 8, 6, 2, 9, 1, 7, 4]  # define uma lista de valores desordenados
 
-pivot
-
-lista_final[]
+print(quick_sort(lista_valores))  # imprime a lista ordenada usando quick_sort
